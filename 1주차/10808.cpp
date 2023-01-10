ccd3eb53978fa98780b9e48be47dfc98fab209b7
HeeSeok <mungml1995@gmail.com>
2023-01-10 22:46:58 +0900

    230110

---
+++ 1주차/10808.cpp
@@ -0,0 +1,24 @@
+/*
+https://www.acmicpc.net/problem/10808 
+*/ 
+#include <bits/stdc++.h>
+using namespace std;
+string S;
+int cnt[26];
+
+int main() {
+	cin >> S;
+	
+	for(char a : S) {
+		cnt[a - 97]++;
+	}
+	
+	for(int i=0; i<26; i++) {
+		cout << cnt[i] << " ";
+	}
+		
+	return 0;
+}
+
+
+
