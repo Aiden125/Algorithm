f5fa0079ec16bccb5bc5c5522501c2111c83d5b6
HeeSeok <mungml1995@gmail.com>
2023-02-17 21:51:42 +0900

    230217

--- 2week/T17298.cpp
+++ 2week/T17298.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int n;
 int a;
+stack<int> st; // 폭탄
 vector<pair<int, int>> v[1000004];
 
 int main(){
@@ -10,15 +11,21 @@ int main(){
     cin >> n;
     for(int i=0; i<n; i++) {
         cin >> a;
-        v[i].push_back({a, -1});
-    
-    }
 
-    for(int i=0; i<n; i++){
-        int b = v[i];
-
-        
+        if(st.empty()){
+            st.push(a);
+        }else{
+            if(st.top() >= a) {
+                st.push(a);
+            }else{
+                while(st.top() < a){
+                    st.pop();
+                }
+            }
+        }
     }
 
     return 0;
 }
+// 1. stack 빈 경우 그냥 push
+// 2. stack
