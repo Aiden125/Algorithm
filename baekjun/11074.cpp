911856620cd2d42973d9ec4cdbcfb6e13c0799bd
HeeSeok <mungml1995@gmail.com>
2023-02-21 21:21:34 +0900

    230221

---
+++ baekjun/11074.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int m, n, a;
+int main(){
+    cin >> n >> m;
+    vector<int> v[n];
+    
+    
+    for(int i=0; i<n; i++){
+        cin >> a;
+        v[i].push_back(a);
+    }
+
+    while(n+1){
+        int b = v[n];
+        
+
+        n--;
+    }
+
+    
+
+    
+
+    return 0;
+}
