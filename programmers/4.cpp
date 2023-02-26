e2a0b68ac350b91a4494917420cc107531cc4a3d
HeeSeok <mungml1995@gmail.com>
2023-02-27 08:56:04 +0900

    230227

---
+++ programmers/4.cpp
@@ -0,0 +1,42 @@
+// 올바른 괄호 https://school.programmers.co.kr/learn/courses/30/lessons/12909
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+
+bool solution(string s){
+    bool answer = true;
+
+    stack<char> st;
+    for(char a : s){
+        cout << a;
+        if(a == '('){
+            st.push(a);
+        }else{
+            if(st.size() && st.top()=='('){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    if(st.size()){
+        return false;
+    }
+
+
+    return answer;
+}
+
+
+int main() {
+    cin >> s;
+    bool ans = solution(s);
+
+    if(ans){
+        cout << "true";
+    }else{
+        cout << "false";
+    }
+
+    return 0;
+}
