e174bd684ce01e77d498283c6d5bba1b294259f8
HeeSeok <mungml1995@gmail.com>
2023-03-16 23:47:03 +0900

    230316

---
+++ CPP/node.cpp
@@ -0,0 +1,13 @@
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
