7f8b75f82ca1ca35d6bf5fc79095c9386465e3db
HeeSeok <mungml1995@gmail.com>
2023-04-18 23:53:46 +0900

    230418

---
+++ Java/src/inflearn/I2_8.java
@@ -0,0 +1,25 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+// 등수구하기 https://cote.inflearn.com/contest/10/problem/02-08
+public class I2_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        ArrayList<Integer, Integer> al = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            arr[i] = a;
+            al.add(a, i+1);
+        }
+        for(int i=0; i<n; i++) {
+
+        }
+
+    }
+}
