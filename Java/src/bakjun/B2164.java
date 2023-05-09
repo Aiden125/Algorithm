adc9ab90efddd2e2d427a066f33bca094f32c1d0
HeeSeok <mungml1995@gmail.com>
2023-05-09 23:54:22 +0900

    230509

---
+++ Java/src/bakjun/B2164.java
@@ -0,0 +1,29 @@
+package bakjun;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class B2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=a; i++) {
+            q.offer(i);
+        }
+
+        int c = 0;
+        while(!q.isEmpty()) {
+            c = q.poll();
+            if(!q.isEmpty()) {
+                int b = q.poll();
+                q.offer(b);
+            }
+        }
+
+        System.out.println(c);
+    }
+}
