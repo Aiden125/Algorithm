485faf9d64f11ba825449c15cccde16f523e94aa
HeeSeok <mungml1995@gmail.com>
2023-05-10 23:58:54 +0900

    230510

---
+++ Java/src/bakjun/B14916.java
@@ -0,0 +1,20 @@
+package bakjun;
+
+import java.util.Scanner;
+
+public class B14916 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        int b = 5;
+        int c = 2;
+        int count = 0;
+
+        while(true) {
+            int d = a%b;
+            count += a/b;
+            if (d!=0)
+        }
+    }
+}
