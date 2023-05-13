eca7c3fb0178002753f89f2d6383f40820aa1908
HeeSeok <mungml1995@gmail.com>
2023-05-13 18:20:51 +0900

    230513

--- Java/src/bakjun/B14916.java
+++ Java/src/bakjun/B14916.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class B14916 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int target = sc.nextInt();
+        int answer = -1;
 
-        int b = 5;
-        int c = 2;
-        int count = 0;
-
-        while(true) {
-            int d = a%b;
-            count += a/b;
-            if (d!=0)
+        for(int i=target/5; i>=0; i--) {
+            int bb = i*5;
+            if (bb == target) {
+                answer = i;
+                break;
+            } else {
+                if ((target - bb)%2 == 0) {
+                    answer = i + (target - bb)/2;
+                    break;
+                }
+            }
         }
+        System.out.println(answer);
+
     }
 }
