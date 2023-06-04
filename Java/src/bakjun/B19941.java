a6285f51203bf1e2c206cef590b32e6da1ed3a18
HeeSeok <mungml1995@gmail.com>
2023-06-04 14:49:26 +0900

    230604

--- Java/src/bakjun/B19941.java
+++ Java/src/bakjun/B19941.java
@@ -8,20 +8,26 @@ public class B19941 {
         int n = sc.nextInt();
         int k = sc.nextInt();
         String s = sc.next();
+        char[] abc = new char[n];
+        for(int i=0; i<n; i++) {
+            abc[i] = s.charAt(i);
+        }
 
         int count = 0;
-        for(int i=0; i<s.length(); i++) {
-            char aaa = s.charAt(i);
+        for(int i=0; i<abc.length; i++) {
+            char aaa = abc[i];
             if (aaa == 'P') {
                 int lt = i-k;
                 int rt = i+k;
                 for(int j=lt; j<=rt; j++){
-                    if (j>0 && j<s.length() && s.charAt(j) == 'H') {
+                    if (j>=0 && j<abc.length && abc[j] == 'H') {
                         count++;
+                        abc[j] = 'X';
                         break;
                     }
                 }
             }
         }
+        System.out.println(count);
     }
 }
---
+++ Java/src/leetcode/L518.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Scanner;
+/**
+ * the hint : https://velog.io/@kai6666/%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-518.-Coin-Change-2-%EC%9E%90%EB%B0%94-%ED%92%80%EC%9D%B4
+dp=[0]*(amount+1)
+        dp[0] = 1
+        for coin in coins:
+        for i in range(1, amount+1):
+        if i - coin >=0:
+        dp[i] += dp[i-coin]
+        return dp[-1]
+ */
+public class L518 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+
+}
