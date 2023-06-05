9be129c104ba0bba33941c86eb3c0d0df2803b14
HeeSeok <mungml1995@gmail.com>
2023-06-05 22:49:41 +0900

    230605

--- Java/src/leetcode/L518.java
+++ Java/src/leetcode/L518.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.Scanner;
 /**
  * the hint : https://velog.io/@kai6666/%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-518.-Coin-Change-2-%EC%9E%90%EB%B0%94-%ED%92%80%EC%9D%B4
@@ -12,10 +13,19 @@ dp=[0]*(amount+1)
         return dp[-1]
  */
 public class L518 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//    }
+    public int change(int amount, int[] coins) {
+        int[] arr = new int[amount+1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        for(int c : coins) {
+            for(int j=c; j<=amount; j++) {
+                arr[j] += arr[j-c];
+            }
+        }
+        return arr[amount];
     }
-
-
 }
