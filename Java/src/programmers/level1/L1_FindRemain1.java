1f6dca92f9348fa49c773929abf668e214e5a9d6
HeeSeok <mungml1995@gmail.com>
2023-06-14 23:00:46 +0900

    230614_L1_FindRemain1

--- Java/src/programmers/level1/L1_FindRemain1.java
+++ Java/src/programmers/level1/L1_FindRemain1.java
@@ -1,4 +1,20 @@
 package programmers.level1;
 
 public class L1_FindRemain1 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=1; i<n; i++) {
+            if (n%i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
 }
