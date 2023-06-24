c4d651503415c8e3c04841ac5a14187d78bf57e5
HeeSeok <mungml1995@gmail.com>
2023-06-24 12:54:37 +0900

    L1_Divisor

--- Java/src/programmers/level1/L1_Divisor.java
+++ Java/src/programmers/level1/L1_Divisor.java
@@ -1,24 +1,25 @@
 package programmers.level1;
 
+// 프1 - 약수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/12928
 public class L1_Divisor {
     public static void main(String[] args) {
 
-//        int n = 16;
-//        System.out.println(solution(n)); // expected: 31
-        int n2 = 0;
-        System.out.println(solution(n2)); // expected: 0
+        int n = 16;
+        System.out.println(solution(n)); // expected: 31
+
     }
     public static int solution(int n) {
         int answer = 0;
-
-        for(int i=1; i<=Math.sqrt(n); i++) {
-            if (n%i==0) {
-                int b = n/i;
-                // 약수가 서로 같은 경우 두번 연산 안하기 위해
-                if (b == i) {
-                    answer += i;
+        // 특정숫자 = 작은파트너 * 큰파트너
+        // 작은파트너가 큰파트너를 넘어가는 시점은 계산할 필요x
+        for(int smallNum=1; smallNum<=Math.sqrt(n); smallNum++) {
+            if (n%smallNum==0) { // 약수인지 확인
+                int bigNum = n/smallNum;
+                if (bigNum == smallNum) {
+                    answer += smallNum;
                 } else {
-                    answer += i + b;
+                    answer += smallNum + bigNum;
                 }
             }
         }
