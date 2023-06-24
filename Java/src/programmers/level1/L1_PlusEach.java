7fde27409e91c42752bbfc49c4e4390682f25275
HeeSeok <mungml1995@gmail.com>
2023-06-24 13:13:48 +0900

    L1_PlusEach

--- Java/src/programmers/level1/L1_PlusEach.java
+++ Java/src/programmers/level1/L1_PlusEach.java
@@ -1,5 +1,7 @@
 package programmers.level1;
 
+// 자릿수 더하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12931
 public class L1_PlusEach {
     public static void main(String[] args) {
 
@@ -14,6 +16,19 @@ public class L1_PlusEach {
         for(int i=0; i<a.length(); i++) {
             answer += Integer.parseInt(String.valueOf(a.charAt(i)));
         }
+
+        return answer;
+    }
+
+    public static int solution_2(int n) {
+        int answer = 0;
+
+        while (n>=10) {
+            int singleDigit = n - ((n/10) * 10);
+            answer += singleDigit;
+            n = n/10;
+        }
+        answer += n;
 
         return answer;
     }
