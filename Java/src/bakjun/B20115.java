5c257a29fa6fee81bd04faf963e2b0ebb591c07f
HeeSeok <mungml1995@gmail.com>
2023-08-27 23:52:14 +0900

    230827

---
+++ Java/src/bakjun/B20115.java
@@ -0,0 +1,19 @@
+package bakjun;
+
+import java.util.Scanner;
+
+public class B20115 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0;
+        double max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            sum += a;
+            max = Math.max(a, max);
+        }
+        double result = ((sum - max)/2) + max;
+        System.out.println(result);
+    }
+}
