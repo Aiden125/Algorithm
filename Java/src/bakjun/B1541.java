1a982db2a8e87643ab9b5ef89279cf2889a2ec81
HeeSeok <mungml1995@gmail.com>
2023-08-28 23:58:34 +0900

    20230828

---
+++ Java/src/bakjun/B1541.java
@@ -0,0 +1,17 @@
+package bakjun;
+
+import java.util.Scanner;
+
+public class B1541 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String a[] = str.split("-");
+        for(int i=0; i<a.length; i++) {
+            String b[] = a[i].split("+");
+            for(int j=0; j<b.length; j++) {
+                
+            }
+        }
+    }
+}
