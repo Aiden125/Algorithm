d25e7864c5119f753caeb43388cb1f2454bd269c
HeeSeok <mungml1995@gmail.com>
2023-11-07 23:53:08 +0900

    231107

---
+++ Java/src/bakjun/datastructure/B2075.java
@@ -0,0 +1,11 @@
+package bakjun.datastructure;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class B2075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        PriorityQueue
+    }
+}
