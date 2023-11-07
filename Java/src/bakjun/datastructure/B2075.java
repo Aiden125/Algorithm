7dcfd1d12aa9027a7b4514f9128e84f2460daf26
HeeSeok <mungml1995@gmail.com>
2023-11-08 03:07:11 +0900

    231108

--- Java/src/bakjun/datastructure/B2075.java
+++ Java/src/bakjun/datastructure/B2075.java
@@ -1,11 +1,23 @@
 package bakjun.datastructure;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class B2075 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        PriorityQueue
+
+        int n = sc.nextInt();
+        int[] arr = new int[n*n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                arr[(i*n)+j] = sc.nextInt();
+            }
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[arr.length-n]);
     }
 }
