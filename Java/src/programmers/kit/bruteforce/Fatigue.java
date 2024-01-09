dd39565a656550cf1a187f8e50f0467dce1ab52b
HeeSeok <mungml1995@gmail.com>
2024-01-10 00:08:13 +0900

    240110

---
+++ Java/src/programmers/kit/bruteforce/Fatigue.java
@@ -0,0 +1,15 @@
+package programmers.kit.bruteforce;
+
+public class Fatigue {
+    public static void main(String[] args) {
+
+        int a = 80;
+        int[][] b = {{80,20}, {50,40}, {30, 10}};
+        System.out.println(Fatigue.solution(a, b)); // 3
+    }
+
+    public static int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
+}
