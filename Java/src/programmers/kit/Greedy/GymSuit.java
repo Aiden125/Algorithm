288135fc7f16ca44618abf2450c95fe150b6de86
HeeSeok <mungml1995@gmail.com>
2024-01-12 01:36:53 +0900

    240112

--- Java/src/programmers/kit/Greedy/GymSuit.java
+++ Java/src/programmers/kit/Greedy/GymSuit.java
@@ -1,31 +1,44 @@
 package programmers.kit.Greedy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GymSuit {
     public static void main(String[] args) {
         int n = 5;
-        int[] lost = {2,4};
-        int[] reserve = {1,3,5};
+        int[] lost = {4,5};
+        int[] reserve = {3,4};
         System.out.println(GymSuit.solution(n, lost, reserve));
     }
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         int[] arr = new int[n];
-        ArrayList<Integer> reserveList = new ArrayList<>();
+        Arrays.sort(lost);
+
+        for (int i=0; i<n; i++) {
+            arr[i] = 1;
+        }
+
+        for (int i=0; i<lost.length; i++) {
+            arr[lost[i]-1] -= 1;
+        }
+
         for (int i=0; i<reserve.length; i++) {
-            boolean check = false;
-            for (int j=0; j<lost.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    check = true;
-                }
-            }
-            if (!check) {
-                reserveList.add(reserve[0]);
+            arr[reserve[i]-1] += 1;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (arr[i] == 0 && i > 0 && arr[i-1] > 1) {
+                arr[i-1] -= 1;
+                arr[i] = 1;
+            } else if (arr[i] == 0 && i+1 < n && arr[i+1] >1) {
+                arr[i+1] -= 1;
+                arr[i] = 1;
             }
         }
-        for (int i=0; i<lost.length; i++) {
 
+        for (int i=0; i<n; i++) {
+            if (arr[i] > 0) answer++;
         }
 
         return answer;
