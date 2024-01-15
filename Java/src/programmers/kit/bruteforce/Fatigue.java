42a883607754f7bfd3d349c9f4111d58f3b703cf
HeeSeok <mungml1995@gmail.com>
2024-01-15 23:25:10 +0900

    2401115

--- Java/src/programmers/kit/bruteforce/Fatigue.java
+++ Java/src/programmers/kit/bruteforce/Fatigue.java
@@ -1,25 +1,15 @@
 package programmers.kit.bruteforce;
 
 public class Fatigue {
+    // 방문배열
     static boolean[] visited;
     static int answer = 0;
-    static boolean flag;
+    static boolean flag = false;
     public static void main(String[] args) {
 
-        // 모든 던전 탐험 가능한 경우
-        int k2 = 300;
-        int[][] dungeons2 = {{100, 20}, {150, 30}, {200, 40}};
-        System.out.println(solution(k2, dungeons2));  // 예상 결과: 3
-
-        // 모든 던전 탐험 불가능한 경우
-        int k3 = 80;
-        int[][] dungeons3 = {{100, 30}, {120, 40}, {150, 50}};
-        System.out.println(solution(k3, dungeons3));  // 예상 결과: 0
-
-        // 던전이 한 개인 경우
-        int k4 = 150;
-        int[][] dungeons4 = {{120, 30}};
-        System.out.println(solution(k4, dungeons4));  // 예상 결과: 1  // 예상 결과: 1
+        int k = 80;
+        int[][] d = {{80,20}, {50,40}, {30,10}};
+        System.out.println(Fatigue.solution(k, d)); // 3
     }
 
     public static int solution(int k, int[][] dungeons) {
@@ -28,27 +18,23 @@ public class Fatigue {
         return answer;
     }
 
-    public static void DFS(int depth, int remain, int[][] dungeons) {
-        // 이미 모든 던전을 다 돌 수 있는 케이스를 찾았다면 더 진행할 필요x
+    static void DFS(int depth, int remain, int[][] dungeons) {
         if (flag) {
             return;
         }
-        // 오면 일단 던전 다 돌기
         for (int i=0; i<dungeons.length; i++) {
-            // 방문했거나 피로도가 부족하면 건너뛰기
+            // 방문했거나, 피로도가 부족한 경우는 continue
             if (visited[i] || remain - dungeons[i][0] < 0) {
                 continue;
             }
-
-            // 방문가능하면 방문하고 다음뎁스 넘어가기
-            visited[i] = true;
+            visited[i] = true; // 방문 체크
             int cost = dungeons[i][1];
-            DFS(depth + 1, remain - cost, dungeons);
-            visited[i] = false;
+            DFS(depth+1, remain-cost, dungeons);
+            visited[i] = false; // 방문 해제
         }
-
         answer = Math.max(answer, depth);
         if (answer == dungeons.length) flag = true;
     }
+
 
 }
