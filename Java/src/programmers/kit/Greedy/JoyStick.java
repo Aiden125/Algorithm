d57c5e3376350813b1b2d5b214f9010e79fca5f8
HeeSeok <mungml1995@gmail.com>
2024-01-18 02:19:09 +0900

    240118

---
+++ Java/src/programmers/kit/Greedy/JoyStick.java
@@ -0,0 +1,11 @@
+package programmers.kit.Greedy;
+
+public class JoyStick {
+    public static void main(String[] args) {
+
+    }
+    public int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
