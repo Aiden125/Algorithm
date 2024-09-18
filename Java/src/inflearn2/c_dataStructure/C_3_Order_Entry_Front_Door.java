1dd6e94b5fbb81de35525f7a05c415009cc935be
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2024-09-18 20:26:36 +0900

    240918

--- Java/src/inflearn2/c_dataStructure/C_3_Order_Entry_Front_Door.java
+++ Java/src/inflearn2/c_dataStructure/C_3_Order_Entry_Front_Door.java
@@ -13,30 +13,20 @@ public class C_3_Order_Entry_Front_Door {
         int time = 0;
         int index = 0;
         int count = 0;
-        int emptyTime = 0;
         while (count < arrival.length) {
             while(index < arrival.length && arrival[index] <= time) {
                 if (state[index] == 1) {
                     outQ.offer(index);
-                    if (emptyTime > 0) {
-                        stateSwitch = 1;
-                        emptyTime = 0;
-                    }
                 } else {
                     inQ.offer(index);
-                    if (emptyTime > 0) {
-                        stateSwitch = 0;
-                        emptyTime = 0;
-                    }
                 }
                 index++;
             }
 
-            if (inQ.isEmpty() && outQ.isEmpty()) {
-                emptyTime++;
-            } else {
-                emptyTime = 0;
+            if (inQ.isEmpty() && outQ.isEmpty()) { // 현재 들어오지도, 나가지도 않는다면 다음에는 나가는 사람을 우선순위로 적용
+                stateSwitch = 1;
             }
+
             if (stateSwitch == 1) {
                 if (!outQ.isEmpty()) {
                     answer[outQ.poll()] = time;
@@ -66,10 +56,12 @@ public class C_3_Order_Entry_Front_Door {
     public static void main(String[] args) {
         C_3_Order_Entry_Front_Door T = new C_3_Order_Entry_Front_Door();
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
+        System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 0, 1})));
         System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
         /**
          * 0 2 3 1 4 5 8 9
+         * 0 2 3 1 4 5 9 8
          * 3 6 4 7 5 8
          * 2 3 4 5 6 8 11 9 10 12
          */
--- Java/src/inflearn2/c_dataStructure/C_3_Order_Entry_Front_Door_Sample.java
+++ Java/src/inflearn2/c_dataStructure/C_3_Order_Entry_Front_Door_Sample.java
@@ -5,43 +5,42 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class C_3_Order_Entry_Front_Door_Sample {
-    public int[] solution(int[] arrival, int[] state){
+    public int[] solution(int[] arrival, int[] state) {
         Queue<Integer> enter = new LinkedList<>();
         Queue<Integer> exit = new LinkedList<>();
-        int n = arrival.length, prev = 1;
-        int[] answer = new int[n];
-        for(int t = 0, i = 0, cnt = 0; ; t++){
-            if(enter.isEmpty() && exit.isEmpty() && i < n) {
-                if(t < arrival[i]){
-                    t = arrival[i];
-                    prev = 1;
+        int arrivalLength = arrival.length, prevState = 1;
+        int[] answer = new int[arrivalLength];
+        for (int time = 0, index = 0, arrivalCnt = 0; ; time++) {
+            if (enter.isEmpty() && exit.isEmpty() && index < arrivalLength) {
+                if (time < arrival[index]) {
+                    time = arrival[index];
+                    prevState = 1;
                 }
             }
-            while(i < n && arrival[i] <= t) {
-                if (state[i] == 0) enter.offer(i);
-                else exit.offer(i);
-                i++;
+            while (index < arrivalLength && arrival[index] <= time) {
+                if (state[index] == 0) enter.offer(index);
+                else exit.offer(index);
+                index++;
             }
-            if(prev == 1) {
-                if(!exit.isEmpty()) {
-                    answer[exit.poll()] = t;
-                    prev = 1;
+            if (prevState == 1) {
+                if (!exit.isEmpty()) {
+                    answer[exit.poll()] = time;
+                    prevState = 1;
+                } else {
+                    answer[enter.poll()] = time;
+                    prevState = 0;
                 }
-                else{
-                    answer[enter.poll()] = t;
-                    prev = 0;
-                }
-            }else if(prev == 0) {
-                if(!enter.isEmpty()) {
-                    answer[enter.poll()] = t;
-                    prev = 0;
-                }else{
-                    answer[exit.poll()] = t;
-                    prev = 1;
+            } else if (prevState == 0) {
+                if (!enter.isEmpty()) {
+                    answer[enter.poll()] = time;
+                    prevState = 0;
+                } else {
+                    answer[exit.poll()] = time;
+                    prevState = 1;
                 }
             }
-            cnt++;
-            if(cnt == n) break;
+            arrivalCnt++;
+            if (arrivalCnt == arrivalLength) break;
         }
         return answer;
     }
@@ -49,10 +48,12 @@ public class C_3_Order_Entry_Front_Door_Sample {
     public static void main(String[] args) {
         C_3_Order_Entry_Front_Door_Sample T = new C_3_Order_Entry_Front_Door_Sample();
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
+        System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 0, 1})));
         System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
         /**
          * 0 2 3 1 4 5 8 9
+         * 0 2 3 1 4 5 9 8
          * 3 6 4 7 5 8
          * 2 3 4 5 6 8 11 9 10 12
          */
