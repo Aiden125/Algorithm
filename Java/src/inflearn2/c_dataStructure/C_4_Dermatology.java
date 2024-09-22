ddbf078d925da5ef5570e9d89476681b76163c89
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2024-09-22 15:28:22 +0900

    240922

--- Java/src/inflearn2/c_dataStructure/C_4_Dermatology.java
+++ Java/src/inflearn2/c_dataStructure/C_4_Dermatology.java
@@ -8,10 +8,45 @@ public class C_4_Dermatology {
         int M = Integer.parseInt(time.split(":")[1]);
         return (H * 60) + M;
     }
+
+    /**
+     * 문제를 약간 바꿔서 동일 시간에 여러 손님이 올수도 있다고 가정할 경우
+     * @param laser
+     * @param enter
+     * @return
+     */
     public int solution(int[] laser, String[] enter) {
         int answer = 0;
 
-        // 처음부터 다시 진행 필요
+        Queue<Integer> q = new LinkedList<>();
+        int endTime = 0; // 다음 입장 가능 시간
+        int cIndex = 0; // 손님 index
+
+        // 첫손님 q에 집어넣기(레이저 소요 시간)
+        int laserRequireTime = laser[Integer.parseInt(enter[cIndex].split(" ")[1])];
+        q.offer(laserRequireTime);
+        endTime = getTime(enter[cIndex].split(" ")[0]); // endTime으로 바로 하면 안되고 enterTime을 넣어줘야함
+        cIndex++;
+
+        for (int time = endTime; time < 1200; time++) {
+            while (cIndex < enter.length && time == getTime(enter[cIndex].split(" ")[0])) {
+                // 손님 띄엄띄엄 오는 경우 예외처리
+                if (q.isEmpty() && getTime(enter[cIndex].split(" ")[0]) > endTime) {
+                    endTime = getTime(enter[cIndex].split(" ")[0]);
+                }
+
+                laserRequireTime = laser[Integer.parseInt(enter[cIndex].split(" ")[1])];
+                q.offer(laserRequireTime);
+                cIndex++;
+            }
+            if (!q.isEmpty() && time == endTime) {
+                endTime += q.poll(); // 수술실에 넣기, 소요시간 추가
+            }
+
+            answer = Math.max(answer, q.size());
+            System.out.println(time);
+        }
+
         return answer;
     }
 
@@ -20,10 +55,12 @@ public class C_4_Dermatology {
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "11:10 2"}));
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
+        System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:39 1", "10:39 1", "10:39 1", "10:39 1"}));
         /**
          * 3
          * 4
          * 0
+         * 4
          */
     }
 }
---
+++ Java/src/inflearn2/c_dataStructure/C_5_Cpu_schedule.java
@@ -0,0 +1,56 @@
+package inflearn2.c_dataStructure;
+
+import java.util.*;
+
+//CPU 스케쥴링
+public class C_5_Cpu_schedule {
+    public int[] solution(int[][] tasks) {
+        int[] answer = new int[tasks.length];
+
+        int[][] inList = new int[tasks.length][3]; // 도착,소요,작업번호
+        for (int i = 0; i < tasks.length; i++) {
+            int[] a = {tasks[i][0], tasks[i][1], i};
+            inList[i] = a;
+        }
+        Arrays.sort(inList, (a,b) -> Integer.compare(a[0], b[0])); // 첫번째 원소 비교, 두번째는 기존 순서 유지
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]); // 첫번째 원소 비교 후 두번째 원소 비교
+        int endTime = 0;
+        pq.offer(new int[]{inList[0][1], inList[0][2]});
+        endTime = inList[0][0]; // 시작 시간 넣어주는거 까먹지 말기
+        int cIndex = 1;
+        int outIndex = 0;
+
+        for (int time = endTime; time < 200000; time++) {
+            while (cIndex < inList.length && time == inList[cIndex][0]) {
+                // 작업간 시간이 붕뜨는 경우 방지
+                if (pq.isEmpty() && endTime < inList[cIndex][0]) {
+                    endTime = inList[cIndex][0];
+                }
+                pq.offer(new int[]{inList[cIndex][1], inList[cIndex][2]});
+                cIndex++;
+            }
+
+            if (!pq.isEmpty() && time == endTime) {
+                int[] a = pq.poll();
+                endTime += a[0]; // 소요시간
+                answer[outIndex++] = a[1]; // 작업번호
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        C_5_Cpu_schedule T = new C_5_Cpu_schedule();
+        System.out.println(Arrays.toString(T.solution(new int[][]{{2, 3}, {1, 2}, {8, 2}, {3, 1}, {10, 2}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{5, 2}, {7, 3}, {1, 3}, {1, 5}, {2, 2}, {1, 1}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{1, 2}, {2, 3}, {1, 3}, {3, 3}, {8, 2}, {1, 5}, {2, 2}, {1, 1}})));
+        System.out.println(Arrays.toString(T.solution(new int[][]{{999, 1000}, {996, 1000}, {998, 1000}, {999, 7}})));
+        /**
+         * [1,3,0,2,4]
+         * [5,4,2,0,1,3]
+         * [7,0,6,1,4,2,3,5]
+         * [1,3,0,2]
+         */
+    }
+}
