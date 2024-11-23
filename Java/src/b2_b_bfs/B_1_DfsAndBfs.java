b346e1a2d9496e2d68569146f76b7caf14777784
DESKTOP-KVGJO43\mungm <mungml1995@gmail.com>
2024-11-23 18:18:37 +0900

    241123

---
+++ Java/src/b2_b_bfs/B_1_DfsAndBfs.java
@@ -0,0 +1,71 @@
+package b2_b_bfs;
+
+import java.io.*;
+import java.util.*;
+public class B_1_DfsAndBfs {
+    static ArrayList<ArrayList<Integer>> arr;
+    static boolean[] visited;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 정점 개수
+        int m = Integer.parseInt(st.nextToken()); // 간선 개수
+        int v = Integer.parseInt(st.nextToken()); // 탐색 시작점
+
+        visited = new boolean[n+1];
+        arr = new ArrayList<>();
+        for (int i=0; i<=n; i++) {
+            arr.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.get(a).add(b);
+            arr.get(b).add(a);
+        }
+
+        sb = new StringBuilder();
+        DFS(v);
+        sb.append("\n");
+        visited = new boolean[n+1];
+        BFS(v);
+
+        System.out.println(sb);
+    }
+
+    static void DFS(int start) {
+        visited[start] = true;
+        sb.append(start + " ");
+
+        ArrayList<Integer> node = arr.get(start);
+        Collections.sort(node);
+        for (int next : node) {
+            if (visited[next]) continue;
+            DFS(next);
+        }
+    }
+
+    static void BFS(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+        sb.append(start + " ");
+
+        while (!q.isEmpty()) {
+            int a = q.poll();
+            ArrayList<Integer> nodes = arr.get(a);
+            Collections.sort(nodes);
+            for (int next : nodes) {
+                if (visited[next]) continue;
+                sb.append(next + " ");
+                q.offer(next);
+                visited[next] = true;
+            }
+        }
+    }
+}
